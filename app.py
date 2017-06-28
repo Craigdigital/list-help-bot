709ccfc26c48f67ee469b76d8c959438535d9838
weilzhang <weilzhang@ebay.com>
2017-06-28 12:07:21 -0700

    roll back

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def creatDraft(parameters):
     payload = {
         "requestListing": {
             "item": {
-                "title": str(condtion) + str(brand) + str(item) + str(model)
+                "title": item
             },
             "categoryId": categories[item],
             "condition": "1000"
