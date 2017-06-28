b7eeb1790a3a7c852ed5afaaa783ad380abf1f44
weilzhang <weilzhang@ebay.com>
2017-06-28 10:43:04 -0700

    improve titile

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def creatDraft(parameters):
     payload = {
         "requestListing": {
             "item": {
-                "title": item
+                "title": str(condtion) + str(brand) + str(item) + str(model)
             },
             "categoryId": categories[item],
             "condition": "1000"
