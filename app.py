d10a845d4ee505640b66a637b7ce18a79cdf4865
weilzhang <weilzhang@ebay.com>
2017-06-29 14:10:22 -0700

    change url

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def creatDraft(parameters):
     model = parameters.get("model")
 
 
-    url = "http://1f0cb7bf.ngrok.io/experience/consumer_selling/v1/listing_draft/create_and_open?mode=AddItem"
+    url = "http://dddc294c.ngrok.io/experience/consumer_selling/v1/listing_draft/create_and_open?mode=AddItem"
 
     payload = {
         "requestListing": {
@@ -69,7 +69,7 @@ def creatDraft(parameters):
 def updateItem(draftId,data,startPrice):
 
 
-    url = "http://1f0cb7bf.ngrok.io/experience/consumer_selling/v1/listing_draft/" + str(draftId) + "?mode=AddItem"
+    url = "http://dddc294c.ngrok.io/experience/consumer_selling/v1/listing_draft/" + str(draftId) + "?mode=AddItem"
 
     payload = {
         "requestListing": {
@@ -148,7 +148,7 @@ def updateItem(draftId,data,startPrice):
 
 def publishItem(draftId, paypal_account):
 
-    url = "http://1f0cb7bf.ngrok.io/experience/consumer_selling/v1/listing_draft/" + str(draftId) + "/publish?mode=AddItem"
+    url = "http://dddc294c.ngrok.io/experience/consumer_selling/v1/listing_draft/" + str(draftId) + "/publish?mode=AddItem"
 
     payload = {
         "requestListing": {
